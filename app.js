86c2c33efde456ab4eb728900b6ef5bc0ff6f4b3
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-08-06 08:58:39 -0300

    Implementando função de cálculo, e função de mensagmem para funcionamento do código

--- app.js
+++ app.js
@@ -5,12 +5,17 @@
 * Versão: 1.0
 ***********************************************************************************************/
 
-//Menssagens de erros
 
-const INPUT_ERROR = "ERRO: A Entrada não pode estar vazia."
-const TYPE_ERROR = "ERRO: Valor inserido inválido."
 
+
+const juros = require('./modulo/juros')
+const mensagem = require('./modulo/mensagem')
 const readline = require('readline')
+
+const formatador = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+})
 
 const entradaDeDados = readline.createInterface( {
     input: process.stdin,
@@ -21,82 +26,93 @@ entradaDeDados.question('Digite o nome do cliente: ', function(nome) {
     const nomeCliente = nome
 
     if (nomeCliente == '') {
-        console.log(INPUT_ERROR)
+        console.log('ERRO: Nenhum dado foi inserido.')
+        entradaDeDados.close()
+    } else if (typeof(nomeCliente) == typeof(1)) {
+        console.log('ERRO: Um caractere inválido foi inserido.')
         entradaDeDados.close()
     } else {
-
         entradaDeDados.question('Digite o nome do produto: ', function(nome) {
             const nomeProduto = nome
-            if (nomeProduto == '') {
-                console.log(INPUT_ERROR)
+
+            if(nomeProduto == '') {
+                console.log('ERRO: Nenhum dado foi inserido')
+                entradaDeDados.close()
+            } else if (typeof(nomeCliente) == typeof(1)){
+                console.log('ERRO: Um caractere inválido foi inserido.')
                 entradaDeDados.close()
             } else {
-
-                entradaDeDados.question('Digite o valor do produto: ', function(valor) {
+                entradaDeDados.question('Digite o valor do produto: ', function(valor){
                     const valorProduto = valor
 
-                    if (valor < 0 || isNaN(valorProduto)) {
-                        console.log(TYPE_ERROR)
+                    if(valorProduto == '') {
+                        console.log('ERRO: Nenhum dado foi inserido.')
                         entradaDeDados.close()
-                    } else {
-                        entradaDeDados.question('O cliente deseja parcelar a compra dele? (S/N)', function(resposta) {
-                            const respostaCliente = resposta
-                            if (respostaCliente == 'S') {
-                                entradaDeDados.question('Digite a taxa de juros anual: ', function(taxa) {
-                                    const taxaJuros = taxa
+                    } else if (isNaN(valorProduto)) {
+                        console.log('ERRO: Um caractere inválido foi inserido.')
+                        entradaDeDados.close()
+                    } else if (valorProduto <= 0){
+                        console.log('ERRO: O sistema não aceita zero ou números negativos nesta entrada.')
+                        entradaDeDados.close()
+                    } else { 
+                        entradaDeDados.question('Qual a taxa de juros anual? ', function(taxa) {
+                            const taxaJuros = taxa/100
 
-                                    if (taxaJuros == '' || isNaN(taxaJuros) || taxaJuros < 0) {
-                                        console.log(INPUT_ERROR)
+                            if(taxaJuros == '') {
+                                console.log('ERRO: Nenhum dado foi inserido.')
+                                entradaDeDados.close()
+                            } else if (isNaN(taxaJuros)) {
+                                console.log('ERRO: Um caractere inválido foi inserido.')
+                                entradaDeDados.close()
+                            } else if (valorProduto <= 0){
+                                console.log('ERRO: O sistema não aceita zero ou números negativos nesta entrada.')
+                                entradaDeDados.close()
+                            } else { 
+
+                                entradaDeDados.question('Qual a quantidade de parcelas que o cliente deseja pagar por ano?: ', function(parcelas) {
+                                    const qtdParcelas = parcelas
+                                    if(qtdParcelas == '') {
+                                        console.log('ERRO: Nenhum dado foi inserido.')
                                         entradaDeDados.close()
-                                    } else {
-                                        entradaDeDados.question('Digite o numero de acréscimo de juros compostos desejados durante o ano: ', function(juros) {
-                                            const quantidadeJuros = juros
-                                            
-                                            if(quantidadeJuros <= 0 || isNaN(quantidadeJuros) || quantidadeJuros == '' ) {
-                                                console.log(INPUT_ERROR)
+                                    } else if (isNaN(qtdParcelas)) {
+                                        console.log('ERRO: Um caractere inválido foi inserido.')
+                                        entradaDeDados.close()
+                                    } else if (valorProduto <= 0){
+                                        console.log('ERRO: O sistema não aceita zero ou números negativos nesta entrada.')
+                                        entradaDeDados.close()
+                                    } else { 
+                                        entradaDeDados.question('Em quantos anos o cliente deseja pagar essa divida? ', function(anos) {
+                                            const anosDeDivida = anos
+        
+                                            if(anosDeDivida == '') {
+                                                console.log('ERRO: Nenhum dado foi inserido.')
                                                 entradaDeDados.close()
-                                            } else {
-                                                entradaDeDados.question('Digite o tempo que sera pago em meses: ', function(meses) {
-                                                    const quantidadeMeses = meses 
-
-                                                    if (quantidadeMeses == '' || isNaN(quantidadeMeses) || quantidadeMeses <= 0) {
-                                                        console.log(INPUT_ERROR)
-                                                        entradaDeDados.close()
-                                                    } else {
-
-                                                        const resultado = `
-                                                            Nome: ${nomeCliente}
-                                                            Produto: ${nomeProduto}
-                                                            Valor: ${valorProduto}
-                                                            Taxa de juros: ${taxaJuros}
-                                                            Juros anual: ${quantidadeJuros}
-                                                            Tempo para pagar: ${quantidadeMeses} Meses
-                                                            `
-
-                                                        console.log(resultado)
+                                            } else if (isNaN(anosDeDivida)) {
+                                                console.log('ERRO: Um caractere inválido foi inserido.')
+                                                entradaDeDados.close()
+                                            } else if (valorProduto <= 0){
+                                                console.log('ERRO: O sistema não aceita zero ou números negativos nesta entrada.')
+                                                entradaDeDados.close()
+                                            } else { 
+                                                
+                                                const resultado = juros.calcularJurosCompostos(valorProduto, taxaJuros, qtdParcelas, anosDeDivida)
+                                                mensagem.exibirMensagem(nomeCliente, nomeProduto, formatador.format(valorProduto), qtdParcelas, anosDeDivida, resultado)
+                                                
+                                                entradaDeDados.close()
 
 
-                                                        entradaDeDados.close()
 
-
-                                                    }
-
-                                                })
                                             }
-                                        })
+
+                                        }) 
+
                                     }
                                 })
-                            } else {
-                                entradaDeDados.close()
                             }
                         })
                     }
-
                 })
-
             }
-
         })
-
     }
 })
